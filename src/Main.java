29a06c1a9ed4ef3ccbde806c5ecf627859d3e224
YayCover9000 <yassuanf@googlemail.com>
2023-01-04 11:18:12 +0100

    try it for not working

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int n = 3;
+        int aufruf1 = RekursiverAufruf(n);
+    }
+
+    public static int RekursiverAufruf(int n) {
+        int zaehler = 5;
+        for (int i = 0; i < zaehler; i++) {
+            int p = 25;
+            int dings = RekursiverAufruf(n - 1);
+            if (n == 1) {
+                System.out.println("Ausgabe weil Rekusion");
+            }
+        }return zaehler;
     }
 }
